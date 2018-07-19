bb15eb59385abea7ed6b8bc3839edc468fea9737
Dzung Nguyen <ngocdungnguyen@chotot.vn>
2018-07-19 15:12:02 +0700

    add SaveAd

---
+++ components/SaveAd/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+import { saveAd, unsaveAd, getSaveAd, resetMessage } from './actions';
+import reducer from './reducer';
+
+const like = 'https://static.chotot.com.vn/storage/adType/adItem/heart.png';
+const unlike = 'https://static.chotot.com.vn/storage/adType/adItem/heart-active.png';
+
+const Wrapper = styled.button`
+  background: transparent;
+  cursor: pointer;
+  border: none;
+  outline: none;
+`;
+
+const STATE = {
+  like: {
+    icon: { src: like, alt: 'like' },
+    action: saveAd,
+  },
+  unlike: {
+    icon: { src: unlike, alt: 'unlike' },
+    action: unsaveAd,
+  }
+};
+
+
+class SaveAd extends React.Component {
+  handleClick = e => {
+    const { require: { func, cb }, gatewayUrl, onClick, dispatch } = this.props;
+
+    if (onClick) {
+      onClick(this.props);
+    }
+
+    if (func()) {
+      const { active, listId } = this.props;
+      const state = active ? 'unlike' : 'like';
+      const config = STATE[state];
+      const { action } = config;
+      dispatch(action({ listId, gatewayUrl }));
+    } else {
+      cb();
+    }
+  }
+
+  render() {
+    const { active, size } = this.props;
+    const state = active ? 'unlike' : 'like';
+    const config = STATE[state];
+    const { icon } = config;
+    return (
+      <Wrapper onClick={this.handleClick}>
+        <img width={size} src={icon.src} alt={icon.alt} />
+      </Wrapper>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    saveAd: state.saveAd,
+  };
+};
+
+SaveAd.propTypes = {
+  size: PropTypes.number,
+  active: PropTypes.bool,
+  require: PropTypes.shape({
+    func: PropTypes.func,
+    cb: PropTypes.func,
+  }).isRequired,
+}
+
+export default connect(mapStateToProps, null)(SaveAd);
+export { reducer, saveAd, unsaveAd, getSaveAd, resetMessage };
