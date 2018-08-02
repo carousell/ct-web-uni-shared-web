2085dff588b567e72fb208e5c0dbb80097087d9f
vutoan266 <toantran@chotot.vn>
2018-08-02 18:12:49 +0700

    update modal

--- Modal/index.js
+++ Modal/index.js
@@ -25,6 +25,7 @@ const ModalWrapper = styled.div`
     height: ${props => props.fullScreenMobile ? '100%' : null};
     top:  ${props => props.fullScreenMobile ? '0px' : `50vh`};
   }
+  border-radius: ${props => props.borderRadius ? props.borderRadius : null}
 `;
 
 const BackDrop = styled.div`
