7ecc57a86e4bf80addfd6502f59a027deaab2a48
Dzung Nguyen <ngocdungnguyen@chotot.vn>
2018-10-19 11:47:55 +0700

    revert cat description

--- components/CategoryDescription/index.js
+++ components/CategoryDescription/index.js
@@ -2,86 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import Styles from './Styles.scss';
+let statusSeeMore = true;
 
-const STATES = {
-  show: {
-    nextStatus: 'hide',
-    nextHeight: '200px',
-  },
-  hide: {
-    nextStatus: 'show',
-    nextHeight: 'auto',
-  },
+const seeMore = (e) => {
+  e.preventDefault();
+  const elem = document.getElementById('contentSeoCat');
+  const elem1 = document.getElementById('buttonSeeMore');
+  const elem2 = document.getElementById('arrowIcon');
+  if (statusSeeMore) {
+    elem.style = 'height: auto';
+    elem1.innerHTML = 'Thu gọn';
+    elem2.className = 'fa fa-angle-double-up';
+  } else {
+    elem.style = 'height: 200px';
+    elem1.innerHTML = 'Xem thêm';
+    elem2.className = 'fa fa-angle-double-down';
+  }
+  statusSeeMore = !statusSeeMore;
 };
 
-class CategoryDescription extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: STATES['show'].nextStatus,
-      contentHeight: STATES['show'].nextHeight,
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.state.status === 'show') {
-      this.setState({
-        status: STATES[this.state.status].nextStatus,
-        contentHeight: STATES[this.state.status].nextHeight,
-      });
-    }
-  }
-
-  getComponent(status) {
-    return {
-      show: () => (
-        <p id="seeMore" className={Styles.seeMore}>
-          <i id="arrowIcon" className="fa fa-angle-double-up" aria-hidden="true" />
-          <a id="buttonSeeMore" onClick={this.handleClick}>
-            Thu gọn
-          </a>
-        </p>
-      ),
-      hide: () => (
-        <p id="seeMore" className={Styles.seeMore}>
-          <i id="arrowIcon" className="fa fa-angle-double-down" aria-hidden="true" />
-          <a id="buttonSeeMore" onClick={this.handleClick}>
-            Xem thêm
-          </a>
-        </p>
-      )
-    }[status];
-  }
-
-  handleClick = e => {
-    this.setState({
-      status: STATES[this.state.status].nextStatus,
-      contentHeight: STATES[this.state.status].nextHeight,
-    });
-  }
-
-  render() {
-    const { catDescription } = this.props;
-    const { status } = this.state;
-    const ControlComponent = this.getComponent(status);
-
-    return catDescription ? (
-      <div className={cx("col-xs-12 text-justify text-muted", Styles.catDescription)}>
-        <p
-          id="contentSeoCat"
-          style={{ height: this.state.contentHeight }}
-          className={cx(Styles.contentWrap)}
-          dangerouslySetInnerHTML={{ __html: catDescription }}
-        />
-
-        <ControlComponent />
-      </div>
-    ) : null;
-  }
+/*eslint-disable */
+const CategoryDescription = ({ catDescription }) => {
+  return catDescription ? (
+    <div className={cx("col-xs-12 text-justify text-muted", Styles.catDescription)}>
+      <p
+        id="contentSeoCat"
+        className={cx(Styles.contentWrap)}
+        dangerouslySetInnerHTML={{ __html: catDescription }}
+      >
+      </p>
+      <p id="seeMore" className={Styles.seeMore}>
+        <i id="arrowIcon" className="fa fa-angle-double-down" aria-hidden="true"></i>
+        <a id="buttonSeeMore" onClick={e => seeMore(e)} >
+          Xem thêm
+        </a>
+      </p>
+    </div>
+  ) : null;
 };
-
 CategoryDescription.propTypes = {
   catDescription: PropTypes.string
 };
-
 export default CategoryDescription;
