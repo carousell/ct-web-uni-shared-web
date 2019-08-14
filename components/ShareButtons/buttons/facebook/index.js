7ac3611dcfb9d9f0ad9087cbfc0a9fa365e2ffcb
vutoan266 <toantran@chotot.vn>
2019-08-14 11:23:04 +0700

    update Facebook component

--- components/ShareButtons/buttons/facebook/index.js
+++ components/ShareButtons/buttons/facebook/index.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import Button from './../../button'
-const fbAppId = '221564734660253'
+import Button from './../../button';
+const fbAppId = '221564734660253';
 
 export default class Facebook extends Component {
   componentDidMount() {
+    const { facebookAppId } = this.props;
     if (!window.fbAsyncInit) {
-      window.fbAsyncInit = function() {
+      window.fbAsyncInit = function () {
         window.FB.init({
-          appId            : fbAppId,
-          autoLogAppEvents : true,
-          xfbml            : true,
-          version          : 'v2.9'
+          appId: facebookAppId || fbAppId,
+          autoLogAppEvents: true,
+          xfbml: true,
+          version: 'v2.9'
         });
         window.FB.AppEvents.logPageView();
       };
 
-      (function(d, s, id){
+      (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
+        if (d.getElementById(id)) { return; }
         js = d.createElement(s); js.id = id;
         js.src = "//connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
@@ -39,7 +40,8 @@ export default class Facebook extends Component {
     });
   }
 
-  render () {
+  render() {
+    if (this.props.hidden) return null;
     return (
       <Button
         title="facebook"
